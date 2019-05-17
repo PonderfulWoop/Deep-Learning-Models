42daeacd64960becb2fd4c56dfa07deacbfd20ba
Shashank Shirol <34227944+PonderfulWoop@users.noreply.github.com>
2019-05-17 16:19:55 +0530

    div_img_patch

---
+++ patch.py
@@ -0,0 +1,13 @@
+import cv2
+import os
+
+def split_img(img_loc, width, height):
+    if not os.path.exists('patches'):
+        os.makedirs('patches')
+    img = cv2.imread(img_loc)
+    for r in range(0, img.shape[0], width):
+        for c in range(0, img.shape[1], height):
+            cv2.imwrite(f"patches/img{r}_{c}.png", img[r:r+width, c:c+height, :])
+
+
+split_img("crowd.jpg", 64, 64)
